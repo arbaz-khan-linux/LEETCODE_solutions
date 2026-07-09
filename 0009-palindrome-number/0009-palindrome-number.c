9fdb06f48940d1e389b56bfa15307a539096bbbf
Arbaz khan <arbaazkhanbhopal91@gmail.com>
2026-07-09 13:11:58 +0530

    Time: 4 ms (26.46%), Space: 8.9 MB (25.69%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,15 @@
+bool isPalindrome(int x) {
+    if (x < 0) return false;  // negative numbers are not palindrome
+
+    long reversed = 0;
+    int original = x;
+
+    while (x > 0) {
+        int digit = x % 10;
+        reversed = reversed * 10 + digit;
+        x /= 10;
+    }
+
+    return reversed == original;
+    
+}
