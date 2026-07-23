f63139c73473479962400155e97ca0e5a1aa5a88
Arbaz khan <arbaazkhanbhopal91@gmail.com>
2026-07-24 03:54:45 +0530

    Time: 0 ms (100%), Space: 8.8 MB (69.14%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -0,0 +1,16 @@
+char* longestCommonPrefix(char** strs, int strsSize) {
+    if (strsSize == 0) return "";
+
+    static char prefix[201];
+    strcpy(prefix, strs[0]);
+
+    for (int i = 1; i < strsSize; i++) {
+        while (strncmp(prefix, strs[i], strlen(prefix)) != 0) {
+            prefix[strlen(prefix) - 1] = '\0';
+            if (strlen(prefix) == 0)
+                return "";
+        }
+    }
+
+    return prefix;
+}
