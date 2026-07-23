9e937c13a541a73a79992fc9e34821a6f1456e31
Arbaz khan <arbaazkhanbhopal91@gmail.com>
2026-07-24 03:58:17 +0530

    Time: 3 ms (43.98%), Space: 8.6 MB (76.15%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.c
@@ -0,0 +1,21 @@
+char* mergeAlternately(char* word1, char* word2) {
+    int i = 0, j = 0, k = 0;
+
+    char *result = (char *)malloc(strlen(word1) + strlen(word2) + 1);
+
+    while (word1[i] != '\0' && word2[j] != '\0') {
+        result[k++] = word1[i++];
+        result[k++] = word2[j++];
+    }
+
+    while (word1[i] != '\0') {
+        result[k++] = word1[i++];
+    }
+
+    while (word2[j] != '\0') {
+        result[k++] = word2[j++];
+    }
+
+    result[k] = '\0';
+    return result;
+}
